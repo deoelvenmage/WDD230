f56d3fef72d992f33228db12296489358c71f21b
deoelvenmage <deoelvenmage@gmail.com>
2022-05-21 22:59:01 -0600

    Populated content, added Mon and Tues banner

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -10,6 +10,20 @@ const month = d.toLocaleString('default', { month: 'long' })
 const year = d.getFullYear();
 document.querySelector('#today-date').textContent = `${weekday}, ${day} ${month} ${year}`;
 
+if (weekday == "Monday" || weekday == "Tuesday") {
+    const p = document.createElement('p');
+    p.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    p.style.textAlign = 'center';
+    p.style.padding = '20px';
+    p.style.border = '2px solid rgb(154, 127, 21)';
+    p.style.backgroundColor = 'rgb(223, 201, 113)';
+    p.style.width = '100%';
+
+    const header = document.querySelector('header');
+    const top = document.querySelector('.header-logo')
+    header.insertBefore(p, top);
+}
+
 
 // hamburger menu responsiveness
 const hambutton = document.querySelector('.hamnav');
@@ -19,3 +33,7 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+
+//using our weekday stuff, check for Monday or Tuesday, then if yes, create a p node with a class "announcement" and append(o) it to the header. Give it CSS properties if needed; porbably some top and bottom padding, and text centering.
