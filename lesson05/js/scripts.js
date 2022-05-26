29e6a56ec471543a990bbfc8f9c4f86a276145e1
deoelvenmage <deoelvenmage@gmail.com>
2022-05-25 22:29:31 -0600

    Made lesson05

---
+++ lesson05/js/scripts.js
@@ -0,0 +1,23 @@
+//vartime; .list is a list item
+const input = document.querySelector("#favchap")
+const button = document.querySelector("#submitter")
+const list = document.querySelector(".listcontainer")
+
+button.addEventListener('click', () => {
+    if (input.value != "") {
+        const item = document.createElement('li')
+        const deleter = document.createElement('button')
+
+        item.textContent = input.value
+        deleter.textContent = "❌"
+        deleter.addEventListener('click', () => {list.removeChild(item)})
+
+        item.appendChild(deleter)
+        list.appendChild(item)
+        input.focus()
+        input.value = ""
+
+        console.log("Done")
+    }
+    return
+})
