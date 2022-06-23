bf4366f7f2ff02a3aef733bac7a8a65d65ee1869
deoelvenmage <deoelvenmage@gmail.com>
2022-06-22 22:11:02 -0600

    changed url to https

--- lesson10/weather-api.js
+++ lesson10/weather-api.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "http://api.openweathermap.org/data/2.5/forecast?q=Fairbanks&units=Imperial&appid=6a4d06e769f51aa7a4f0a6138ef208c9"
+const url = "https://api.openweathermap.org/data/2.5/forecast?q=Fairbanks&units=Imperial&appid=6a4d06e769f51aa7a4f0a6138ef208c9"
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
